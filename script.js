7b80cf956cf0bb5b2792b531f05ffa2b28bbfb60
Jemery Inoa <jeremyinoa67@gmail.com>
2022-10-24 22:44:12 -0400

    adds the necessary functionality to change the DOM

--- script.js
+++ script.js
@@ -2,26 +2,26 @@
 async function searchPokemon() {
 
     //Sacar input del usuario
-    let userInput= document.getElementById("valorForm").value;
+        let userInput= document.getElementById("valorForm").value;
 
 
     //Recurso para crear URL para conectar con la API
-    function createURL(a){
-        return `https://pokeapi.co/api/v2/pokemon/${a}`
-    }
+        function createURL(a){
+            return `https://pokeapi.co/api/v2/pokemon/${a}`
+        }
     //Prueba de codigo
     // console.log(createURL(userInput))
 
     // Conectar con la API
-    let response = await fetch(`${createURL(userInput)}`);
-    let pokemon = await  response.json();
+        let response = await fetch(`${createURL(userInput)}`);
+        let pokemon = await  response.json();
 
     //Nombre y foto sacadas de la API
-    let pokemonName = pokemon["name"];
-    let pokemonImg = pokemon["sprites"]["front_default"];
+        let pokemonNameAPI = pokemon["name"];
+        let pokemonImgAPI = pokemon["sprites"]["front_default"];
 
     //Stats sacadas de la API
-    let pokemonXP = pokemon["base_experience"]
+        let pokemonXPAPI = pokemon["base_experience"];
 
 
     //Habilidades sacadas de la API
@@ -30,10 +30,32 @@ async function searchPokemon() {
 
 
     //Probar que los valores sean validos
-    console.log(pokemonName)
-    console.log(pokemonImg)
-    console.log(pokemonXP)
+        console.log(pokemonNameAPI)
+        console.log(pokemonImgAPI)
+        console.log(pokemonXPAPI)
     //Aparecer contenedor
-    let element = document.getElementById("PokemonContainer");
-    element.classList.remove("invisible");
-}
+        let element = document.getElementById("PokemonContainer");
+        element.classList.remove("invisible");
+    //Declaracion de valores del DOM
+        //Nombre y foto
+            let pokemonNombreDOM = document.getElementById("pokemonNombre");
+            let pokemonimagenDOM = document.getElementById("pokemonImagen");
+        //Stats sacadas de la API
+            let Stat1DOM = document.getElementById("statUno");
+            let Stat2DOM = document.getElementById("statDos");
+            let Stat3DOM = document.getElementById("statTres");
+            let Stat4DOM = document.getElementById("statCuatro");
+        //Habilidades sacadas de la API
+
+
+
+    //Cambio de valores en el DOM
+        //Nombre y foto
+            pokemonNombreDOM.innerHTML = `${[pokemonNameAPI]}`
+            pokemonimagenDOM.src = `${[pokemonImgAPI]}`
+        //Stats sacadas de la API
+
+        //Habilidades sacadas de la API
+
+
+}   
